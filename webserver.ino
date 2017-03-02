a0a1f96ba636c9f1c9e05a463cd5d48aed619980
Cellie <m.timmer@wasietsmet.nl>
2017-03-02 16:12:34 +0100

    Changed the urldecode() function.
    Now using the urlDecode() that is included in ESP8266WebServer.
    Less code and ESP8266WebServer is already linked in.
    Deleted the old routine.

--- webserver.ino
+++ webserver.ino
@@ -208,7 +208,7 @@ void handleNotFound() {
 }
 
 bool handleSPIFFSfile( String path ) {
-  path = urldecode( path );
+  path = webServer.urlDecode( path );
   if ( path.endsWith( "/" ) ) path += "index.htm";
 
   if ( SPIFFS.exists( path ) ) {
