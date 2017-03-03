32d9c5f3bae39926e8696d087cde29e3646949be
Cellie <m.timmer@wasietsmet.nl>
2017-03-03 14:46:13 +0100

    Refactor getContentType.
    Actually copy pasted from ESP8266webserver.
    Supports more mime types.

--- webserver.ino
+++ webserver.ino
@@ -264,19 +264,27 @@ void showFileUploader() {
   webServer.send( 200, "text/html", uploadHTMLheader + HTTPresponse + uploadForm + uploadHTMLfooter);
 }
 
-String getContentType( String filename ) {
-  if ( webServer.hasArg( "download" )) return "application/octet-stream";
-  else if ( filename.endsWith( ".htm" ) ) return "text/html";
-  else if ( filename.endsWith( ".html" ) ) return "text/html";
-  else if ( filename.endsWith( ".css" ) ) return "text/css";
-  else if ( filename.endsWith( ".js" ) ) return "application/javascript";
-  else if ( filename.endsWith( ".png" ) ) return "image/png";
-  else if ( filename.endsWith( ".gif" ) ) return "image/gif";
-  else if ( filename.endsWith( ".jpg" ) ) return "image/jpeg";
-  else if ( filename.endsWith( ".ico" ) ) return "image/x-icon";
-  else if ( filename.endsWith( ".xml" ) ) return "text/xml";
-  else if ( filename.endsWith( ".pdf" ) ) return "application/x-pdf";
-  else if ( filename.endsWith( ".zip" ) ) return "application/x-zip";
-  else if ( filename.endsWith( ".gz" ) ) return "application/x-gzip";
-  return "text/plain";
+static String getContentType( const String& path) {
+  if (path.endsWith(".html")) return "text/html";
+  else if (path.endsWith(".htm")) return "text/html";
+  else if (path.endsWith(".css")) return "text/css";
+  else if (path.endsWith(".txt")) return "text/plain";
+  else if (path.endsWith(".js")) return "application/javascript";
+  else if (path.endsWith(".png")) return "image/png";
+  else if (path.endsWith(".gif")) return "image/gif";
+  else if (path.endsWith(".jpg")) return "image/jpeg";
+  else if (path.endsWith(".ico")) return "image/x-icon";
+  else if (path.endsWith(".svg")) return "image/svg+xml";
+  else if (path.endsWith(".ttf")) return "application/x-font-ttf";
+  else if (path.endsWith(".otf")) return "application/x-font-opentype";
+  else if (path.endsWith(".woff")) return "application/font-woff";
+  else if (path.endsWith(".woff2")) return "application/font-woff2";
+  else if (path.endsWith(".eot")) return "application/vnd.ms-fontobject";
+  else if (path.endsWith(".sfnt")) return "application/font-sfnt";
+  else if (path.endsWith(".xml")) return "text/xml";
+  else if (path.endsWith(".pdf")) return "application/pdf";
+  else if (path.endsWith(".zip")) return "application/zip";
+  else if(path.endsWith(".gz")) return "application/x-gzip";
+  else if (path.endsWith(".appcache")) return "text/cache-manifest";
+  return "application/octet-stream";
 }
